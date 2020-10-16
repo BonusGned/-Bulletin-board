725b39a14540df626cf4f151e587c7984c65f97a
BonusGned <jesus.moj@mail.com>
2020-10-16 19:28:48 +0300

    paginate, main page

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().filter(staus='published')
+        return super().get_queryset().filter(status='published')
 
 
 class Post(models.Model):
@@ -24,6 +25,12 @@ class Post(models.Model):
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
     objects = models.Manager()
     published = PublishedManager()
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.month, self.publish.day, self.slug])
+
+
 
     class Meta:
         ordering = ('-publish',)
